46b16d6ccab8445c0db64b5bb515f1f0caeef05f
Jack Nolan <johnmnolan97@gmail.com>
2024-01-28 09:08:22 -0500

    Background scroll image save

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-
-
+import '../styles/style.css';
 const Home = () => {
-
-    return (
-        <div>
-            Home
+  return (
+    <div className='Home'>
+      <div class='scrolling-image-container'>
+        <div class='scrolling-image'>
+          <h2 className='TitleText'>Jack's Portfolio Site</h2>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
