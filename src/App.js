3a2742ece9c71c4265778949b1edd317ac74356c
jnolan97 <johnmnolan97@gmail.com>
2024-03-23 05:50:01 -0400

    stashing some initial react MUI stuff

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import './styles/style.css';
 import AboutMe from './components/AboutMe';
-import Resumepdf from './assets/Resumepdf.pdf'
+import Resumepdf from './assets/Resumepdf.pdf';
+import { MDBContainer } from 'mdb-react-ui-kit';
 
 function App() {
   return (
-    <>
-    <div className='header'>
-    <h1>Jack's Portfolio Website</h1>
-    <h3>Welcome to my developer page</h3>
-    <span className='header-links'>
-    <a href={Resumepdf} download='Resume2024'>Download my Resume</a>
-    &nbsp; | &nbsp;
-    <a href='https://www.linkedin.com/in/john-nolan-04062b1a8' target='_blank'>LinkedIn</a>
-    </span>
-
-  </div>
-    <AboutMe />
-    </>
+    <MDBContainer>
+      <div className='header'>
+        <h1>Jack Nolan - Software Engineer</h1>
+        <h3>Welcome to my portfolio site</h3>
+        <span className='header-links'>
+          <a href={Resumepdf} download='Resume2024'>
+            Download my Resume
+          </a>
+          &nbsp; | &nbsp;
+          <a
+            href='https://www.linkedin.com/in/john-nolan-04062b1a8'
+            target='_blank'
+            rel='noreferrer'
+          >
+            LinkedIn
+          </a>
+        </span>
+      </div>
+      <AboutMe />
+    </MDBContainer>
   );
 }
 
--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,41 +1,49 @@
 import React from 'react';
-
+import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 const AboutMe = () => {
   return (
-    <div className='content'>
-      <div class='column-2'>
-        <h1>About me</h1>
-        <p>
-          Hi! My name is Jack Nolan. Welcome to my portfolio site. Here I've highlighted some of my
-          professional accomplishments and technical strengths, as well as underline my overall
-          interests and future goals.
-        </p>
-        <p>
-          Below I go into greater detail about the languages, frameworks, and tools that I am
-          experienced with and provide references to situations where I was able to excersize those
-          abilities and go into some of the improvements and gains I have been able to accomplish
-          for the companies I have worked for. I also took some time to write about my interests and
-          personal hobbies, as well as what I value in myself and the teams I work with.
-        </p>
-      </div>
-      <div class='column-3'>
-        <h1>Top Skills</h1>
-        <ul>
-          <li>Python</li>
-          <li>JavaScript / TypeScript</li>
-          <li>.NET Framework / .NET Core</li>
-          <li>SQL - Relational Database Architecture, Management, Optimization</li>
-          <li>
-            React - Proficient in up to date best practices with functional components, Context, and
-            Redux
-          </li>
-          <li>Testing Libraries: Jest, NUnit, Pytest</li>
-          <li>Cloud Computing and DevOps Integrations (AWS, Azure, Docker)</li>
-          <li>Authentication and Authorization architectures (SAML v2 SSO, Oath2, etc...)</li>
-          <li>Git version control</li>
-        </ul>
-      </div>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <h1>About me</h1>
+          <p>
+            Hi! My name is Jack Nolan. Welcome to my portfolio site. Here I've highlighted some of
+            my professional accomplishments and technical strengths, as well as underline my overall
+            interests and future goals.
+          </p>
+          <p>
+            Below I go into greater detail about the languages, frameworks, and tools that I am
+            experienced with and provide references to situations where I was able to excersize
+            those abilities and go into some of the improvements and gains I have been able to
+            accomplish for the companies I have worked for. I also took some time to write about my
+            interests and personal hobbies, as well as what I value in myself and the teams I work
+            with.
+          </p>
+        </Grid>
+
+        <Grid item xs={6}>
+          <div className='text-center'>
+            <h1>Top Skills</h1>
+            <ul>
+              <li>Python</li>
+              <li>JavaScript / TypeScript</li>
+              <li>.NET Framework / .NET Core</li>
+              <li>SQL - Relational Database Architecture, Management, Optimization</li>
+              <li>
+                React - Proficient in up to date best practices with functional components, Context,
+                and Redux
+              </li>
+              <li>Testing Libraries: Jest, NUnit, Pytest</li>
+              <li>Cloud Computing and DevOps Integrations (AWS, Azure, Docker)</li>
+              <li>Authentication and Authorization architectures (SAML v2 SSO, Oath2, etc...)</li>
+              <li>Git version control</li>
+            </ul>
+          </div>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
